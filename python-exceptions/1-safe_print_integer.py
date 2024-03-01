637ffb94741ac7d03935ee1c6f693b89c184fcae
ulrichr7 <ulrichr7@gmail.com>
2024-03-01 09:14:00 -0800

    opo

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    elements = 0
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            print(my_list[i], end='')
-            elements += 1
-    except:
-        pass
-    print()
-    return elements
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
