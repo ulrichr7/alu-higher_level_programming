e7e23ffc623f989ac88729f98c9aeb8a4f2ab879
ulrichr7 <ulrichskyler7@gmail.com>
2024-03-24 20:56:57 +0000

    lkl

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """ Pythonn script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
