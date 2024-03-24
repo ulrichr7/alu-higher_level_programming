1231760653ffb5f92615733cc4861f0f7ab64cf0
ulrichr7 <ulrichskyler7@gmail.com>
2024-03-24 21:41:08 +0000

    pop

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
+"""fetches https://intranet.hbtn.io/status using requests"""
 import requests
 
-if __name__ == '__main__':
-    res = requests.get("https://intranet.hbtn.io/status")
+
+if __name__ == "__main__":
+    """fetches https://intranet.hbtn.io/status using requests"""
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    if res.text.strip() == "OK":
-        print("\t- content: OK")
-    else:
-        print("\t- content:", res.text)
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
